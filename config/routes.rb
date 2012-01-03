8004e92a2afa30f45b3cea7191dda38779722b3e
Steph Skardal <steph@endpoint.com>
2012-01-02 21:07:28 -0500

    Remove routes update.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Piggybak::Engine.routes.draw do
     match "/cart" => "cart#show", :as => :cart
     match "/cart/add" => "cart#add", :via => :post, :as => :cart_add
     match "/cart/update" => "cart#update", :via => :post, :as => :cart_update
-    match "/cart/remove/:item" => "cart#remove", :via => :delete
+    match "/cart/remove/:item" => "cart#remove", :via => :delete, :as => :remove_item
     match "/submit" => "orders#submit", :via => :post, :as => :order_submit
     match "/receipt" => "orders#receipt", :as => :receipt
     match "/orders" => "orders#list", :as => :orders_list

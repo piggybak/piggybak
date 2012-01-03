6598b2e98b8f2d1d40476e652ead2ac14182af10
Brian Buchatler <brianb@endpoint.com>
2012-01-03 15:16:26 -0500

    Change validation to verify both parts of polymorphic relationship are present.  Added :item accessor to allow direct assignment of Product#item.

--- app/models/piggybak/product.rb
+++ app/models/piggybak/product.rb
@@ -1,12 +1,13 @@
 class Piggybak::Product < ActiveRecord::Base
   belongs_to :item, :polymorphic => true, :inverse_of => :piggybak_product
-  attr_accessible :sku, :description, :price, :quantity, :active, :unlimited_inventory, :item_id, :item_type
+  attr_accessible :sku, :description, :price, :quantity, :active, :unlimited_inventory, :item_id, :item_type 
+  attr_accessible :item # to allow direct assignment from code or console
 
   validates_presence_of :sku
   validates_uniqueness_of :sku
   validates_presence_of :description
   validates_presence_of :price
-  validates_presence_of :item_type
+  validates_presence_of :item
   validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 0
     
   def admin_label

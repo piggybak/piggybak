1a93fa33d4bfb143f76c18daff0c50bbb22f50ea
Steph Skardal <steph@endpoint.com>
2012-01-11 23:00:42 -0500

    More test framework troubleshooting.

--- spec/dummy_app/config/routes.rb
+++ spec/dummy_app/config/routes.rb
@@ -1,10 +1,8 @@
 DummyApp::Application.routes.draw do
   mount Piggybak::Engine => '/demo/checkout', :as => 'piggybak'
 
-  devise_for :users
-
-  match "/demo/" => 'home#index', :as => :root
-  match '/demo/image/:id' => 'images#show', :as => :image
-  match '/demo/c/:id' => 'categories#show', :as => :category
-  match '/demo/:slug' => 'pages#show'
+  match "/" => 'home#index', :as => :root
+  match '/image/:id' => 'images#show', :as => :image
+  match '/c/:id' => 'categories#show', :as => :category
+  match '/:slug' => 'pages#show'
 end
--- spec/dummy_app/db/migrate/20111231040354_create_images.rb
+++ spec/dummy_app/db/migrate/20111231040354_create_images.rb
@@ -6,6 +6,11 @@ class CreateImages < ActiveRecord::Migration
       t.text :description
       t.references :user, :null => false
 
+      t.string :main_file_name
+      t.string :main_content_type
+      t.string :main_file_size
+      t.datetime :main_updated_at
+
       t.boolean :is_featured, :null => false, :default => false
 
       t.timestamps

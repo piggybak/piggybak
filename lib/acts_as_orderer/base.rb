81eacf9ed19c08411ba9cf17d2f18ab6bd671e6d
Steph Skardal <steph@endpoint.com>
2012-03-05 10:39:22 -0500

    Clean up dummy method.

--- lib/acts_as_orderer/base.rb
+++ lib/acts_as_orderer/base.rb
@@ -10,18 +10,8 @@ module Piggybak
       module ClassMethods
         def acts_as_orderer
           has_many :piggybak_orders, :foreign_key => "user_id", :class_name => "::Piggybak::Order"
-
-          include Piggybak::ActsAsOrderer::Base::InstanceMethods
         end
       end
-      
-      module InstanceMethods
-        
-        def factory_name
-          "this is an example instance method"
-        end
-                
-      end # InstanceMethods      
     end
   end
 end

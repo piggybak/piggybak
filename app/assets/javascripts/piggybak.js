cbe25838b443538e07ed34d1eddd0dcc40eb7d07
Tim Case <tim@powerupdev.com>
2012-10-06 03:10:00 +0700

    make submit disable for when order is submitted

--- app/assets/javascripts/piggybak.js
+++ app/assets/javascripts/piggybak.js
@@ -16,10 +16,9 @@ $(function() {
 
 var piggybak = {
 	prevent_double_click: function() {
-		//TODO: Implement doubleclick prevent here on form submission
-		//$('#new_piggybak_order').submit(function() {
-		//disable if already submitted
-		//});
+    $('#new_piggybak_order').submit(function() {
+      $(this).find('input:submit').attr('disabled', 'disabled');
+    });
 	},
 	initialize_listeners: function() {
 		shipping_els.live('change', function() {

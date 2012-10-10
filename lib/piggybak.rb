49663b5316617b609bf8866f5939af9e0d22a3ab
Tim Case <tim@powerupdev.com>
2012-10-11 00:45:04 +0700

    Add conditionals for User class existing

--- lib/piggybak.rb
+++ lib/piggybak.rb
@@ -60,7 +60,8 @@ module Piggybak
             field :created_at
             field :email
             field :phone
-            field :user
+
+            field :user if defined?(User)
 
             field :line_items
             field :billing_address
@@ -122,7 +123,7 @@ module Piggybak
               end
             end
 
-            field :user
+            field :user if defined?(User)
             field :email
             field :phone
             field :ip_address do
@@ -163,7 +164,7 @@ module Piggybak
           object_label_method :admin_label
           visible false
           list do
-            field :user
+            field :user if defined?(User)
             field :note
             field :created_at
           end
